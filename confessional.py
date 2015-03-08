94e82e3a20767fe1cac36f48cb2d0ef88d9d91cb
Roopa Vasudevan <roopa.vasudevan@gmail.com>
2015-03-08 19:21:57 -0400

    unique word counting completed

--- confessional.py
+++ confessional.py
@@ -2,6 +2,10 @@ from os import system
 import speech_recognition as sr
 
 r = sr.Recognizer()
+# r.pause_threshold = 0.2
+
+counted = []
+indices = []
 
 #accesses the microphone and begins listening
 with sr.Microphone() as source:
@@ -12,14 +16,29 @@ try:
 	say = r.recognize(audio)
 
 	#begin text processing
-	say = say.lower()
+	say = say.lower() #convert everything to lowercase to avoid duplicates
 	words = say.split(" ")
 	for w in words:
-		print w
+		# print w
 
-	say = 'say ' + 'You said ' + say
-	system(say)
+		# if the word exists already
+		if w in counted:				
+			print "Found a duplicate!"
+			index = counted.index(w)
+
+			#increase the count
+			indices[index] += 1
+
+		#otherwise
+		else:
+
+			#add it to both lists
+			counted.append(w)
+			indices.append(1)
 
 except LookupError:
 	#if the computer can't understand what was said
 	print("Could not understand audio")
+
+print counted
+print indices
